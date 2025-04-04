afe0552ef0a1ccbacb283733ce699c2d7cd644d7
Thembelihle26 <167672275+Thembelihle26@users.noreply.github.com>
2025-04-04 17:13:18 +0000

    Added Voice Greeting

--- Cyberwisebot/Program.cs
+++ Cyberwisebot/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 
 namespace Cyberwisebot
 {
@@ -10,6 +13,15 @@ namespace Cyberwisebot
     {
         static void Main(string[] args)
         {
+            PlayVoiceGreeting();
+
         }
+        static void PlayVoiceGreeting()
+        {
+           SoundPlayer soundPlayer = new SoundPlayer("greeting.wav"); 
+            soundPlayer.Load();
+            soundPlayer.PlaySync();
+        }
+
     }
 }
